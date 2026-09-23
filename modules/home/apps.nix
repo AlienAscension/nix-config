150574f3fd8e2994efd47a4ebcee9a9c8727dc6d
AlienAscension <148532831+AlienAscension@users.noreply.github.com>
2026-09-23 14:27:30 +0200

    add okular and set it as default PDF viewer

--- modules/home/apps.nix
+++ modules/home/apps.nix
@@ -10,7 +10,22 @@
     libreoffice
     flameshot
     steam
+    kdePackages.okular
   ];
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/pdf" = "org.kde.okular.desktop";
+      "image/jpeg" = "firefox.desktop";
+      "image/png" = "firefox.desktop";
+    };
+    associations.added = {
+      "application/pdf" = [ "org.kde.okular.desktop" ];
+      "image/jpeg" = [ "firefox.desktop" ];
+      "image/png" = [ "firefox.desktop" ];
+    };
+  };
 
   xdg.configFile."flameshot/flameshot.ini".text = ''
     [General]
