5f561bc752893a398f84a35b123f9ad98daf8eca
AlienAscension <linus.breitenberger@gmail.com>
2026-09-25 11:03:43 +0200

    enable agenix for macbook

--- machines/macbook/secrets.nix
+++ machines/macbook/secrets.nix
@@ -1,16 +1,16 @@
 {
-  # agenix secret declarations for this host
-  # Uncomment after first activation when the SSH host key exists.
-  # age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-  #
-  # age.secrets = {
-  #   ssh-personal-git = {
-  #     file = ../../secrets/ssh-personal-git.age;
-  #     owner = "lbr";
-  #     group = "staff";
-  #     mode = "0600";
-  #   };
-  #
+   agenix secret declarations for this host
+   Uncomment after first activation when the SSH host key exists.
+   age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  
+   age.secrets = {
+     ssh-personal-git = {
+       file = ../../secrets/ssh-personal-git.age;
+       owner = "lbr";
+       group = "staff";
+       mode = "0600";
+     };
+  
   #   ssh-personal-homelab = {
   #     file = ../../secrets/ssh-personal-homelab.age;
   #     owner = "lbr";
